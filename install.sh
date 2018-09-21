abd0fb274b763f03e6e90d8176e54d81c95b385d
astrugatch <22604593+astrugatch@users.noreply.github.com>
2018-09-21 08:19:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-VERSION=1.7.0
+VERSION=2.4.4
 URL=https://github.com/digitalocean/netbox/archive/v${VERSION}.tar.gz
 CURDIR=`pwd`
+POSTGRESPWD=N3TB0XD@T@B@S3
+PRIVATE_KEY='Yb3QMW1VI6Ln8xUWLqZc7h3G4csdouHDTq5zclbES9EtjIclyM'
 
 # Install pre-requisites
-sudo apt-get install -y postgresql libpq-dev python-psycopg2
+sudo apt-get install -y postgresql libpq-dev
 
 # Setup postgres
 sudo -u postgres psql < ${CURDIR}/conf/postgres.conf
 
 # Install app pre-requisites
-sudo apt-get install -y python2.7 python-dev python-pip libxml2-dev libxslt1-dev libffi-dev graphviz libpq-dev libssl-dev
-sudo pip install --upgrade pip
+sudo apt-get install -y python3 python3-dev python3-setuptools build-essential libxml2-dev libxslt1-dev libffi-dev graphviz libpq-dev libssl-dev zlib1g-dev
+sudo easy_install3 pip
 
 # Download and install app
 mkdir /tmp/netbox/
@@ -23,29 +25,31 @@ sudo ln -s /opt/netbox-${VERSION} /opt/netbox
 
 # Install app requirements
 cd /opt/netbox/
-sudo pip install -r requirements.txt
+sudo pip3 install -r requirements.txt
 
 # Setup configuration
 cd netbox/netbox/
 sudo cp configuration.example.py configuration.py
 sudo sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['*'\]/" /opt/netbox/netbox/netbox/configuration.py
 sudo sed -i "s/'USER': '',/'USER': 'netbox',/" /opt/netbox/netbox/netbox/configuration.py
-sudo sed -i "s/'PASSWORD': '',           # PostgreSQL password/'PASSWORD': 'somethingsomethingsomethingdarkside',/" /opt/netbox/netbox/netbox/configuration.py
-PRIVATE_KEY='aslknfdslakfn3q43qknSKNDKNalisjf23jnlknd2kdn2dsknasdKN'
+sudo sed -i "s/'PASSWORD': '',           # PostgreSQL password/'PASSWORD': '{POSTGRESPWD}',/" /opt/netbox/netbox/netbox/configuration.py
 sudo sed -i "s/SECRET_KEY = ''/SECRET_KEY = '${PRIVATE_KEY}'/" /opt/netbox/netbox/netbox/configuration.py
 
 # Run database migrations
 cd /opt/netbox/netbox/
-sudo python manage.py migrate
+sudo python3 manage.py migrate
 
 # Create super user
-sudo python manage.py createsuperuser
+sudo python3 manage.py createsuperuser
 
 # Collect static files
-sudo python manage.py collectstatic
+sudo python3 manage.py collectstatic
 
 # Install webservers
-sudo apt-get install -y gunicorn supervisor nginx
+sudo apt-get install -y supervisor nginx
+
+# Install gunicorn
+sudo pip3 install gunicorn
 
 # Configure webservers
 sudo cp ${CURDIR}/conf/nginx_netbox.conf /etc/nginx/sites-available/netbox.conf
